861a6ec6e536d140e497a8ad838344b89efef866
Robert-0410 <robertbeltran@pm.me>
2023-07-01 00:27:55 -0700

    added nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,16 @@
 -- See the kickstart.nvim README for more information
 --
 -- vim general settings trying to get out of the way for future merges from the parent repo.
+--
+-- NeoTree Recomendations
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
 
+-- Persennal
 vim.o.relativenumber = true
+
+
+vim.keymap.set('n', '<leader>m', '<C-w>')
+
 return {}
---
+++ lua/custom/plugins/nvimtree.lua
@@ -0,0 +1,11 @@
+vim.keymap.set('n', '<leader>t', '<Cmd>NvimTreeToggle<CR>')
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
