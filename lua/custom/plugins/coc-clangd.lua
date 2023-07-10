eef9369596228b2c424738b862b3f9bb9f4810e6
Robert-0410 <robertbeltran@pm.me>
2023-07-10 15:43:16 -0700

    added clangd for aarch64

---
+++ lua/custom/plugins/coc-clangd.lua
@@ -0,0 +1,31 @@
+
+-- Had to move into the coc.nvim dir and call: npm install
+-- Had to install clangd-12 using npm and add the path for clangd-12. See the respective repos
+--
+-- This should only run on raspberry since Mason does not provide clangd for the platform
+
+local platform = vim.loop.os_uname().machine
+
+if (platform == "aarch64")
+then
+  print("Im the PI")
+  local P = {
+    "neoclide/coc.nvim",
+    branch = "master",
+  }
+
+  P.config = function ()
+    vim.opt.backup = false
+    vim.opt.writebackup = false
+
+    vim.opt.updatetime = 300
+    vim.opt.signcolumn = "yes"
+  end
+
+  return P
+
+else
+  return{}
+end
+
+
