9a09cf701e9c33e94d3efa724b70cd57954b5e58
robert <robertbeltran@pm.me>
2024-06-20 18:54:45 -0700

    added spell check stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,15 +5,21 @@
 --
 -- vim general settings trying to get out of the way for future merges from the parent repo.
 --
--- NeoTree Recomendations
+-- NeoTree Recommendations
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
--- Persennal
+-- Personnel
 vim.o.relativenumber = true
 
+-- Spell checking
+vim.opt.spelllang = 'en_us'
+vim.keymap.set('n', '<Leader>p', function()
+  vim.opt.spell = not vim.opt.spell:get()
+end)
 
+-- Key mapping
 vim.keymap.set('n', '<leader>m', '<C-w>')
 
 return {}
